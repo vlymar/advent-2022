c1c963e3e08219534602ae18eb427dbc141d8c79
Victor Lymar <victorslymar@gmail.com>
2022-12-10 12:22:46 -0800

    part2 done

---
+++ 1/part2/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+)
+
+// slow, but my computer is fast
+func keep3RichestElves(richestElves []int, candidateCalories int) []int {
+	richestElves = append(richestElves, candidateCalories)
+
+	sort.Ints(richestElves)
+
+	if len(richestElves) > 3 {
+		richestElves = richestElves[1:4]
+	}
+
+	return richestElves
+}
+
+func sumCalories(richestElves []int) int {
+	sum := 0
+	for _, c := range richestElves {
+		sum += c
+	}
+	return sum
+}
+
+func main() {
+	f, err := os.Open("../input")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	richestElves := []int{}
+
+	currElfNum := 1
+	currElfCalories := 0
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		if text == "" {
+			richestElves = keep3RichestElves(richestElves, currElfCalories)
+
+			currElfNum += 1
+			currElfCalories = 0
+		} else {
+			val, err := strconv.Atoi(text)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			currElfCalories += val
+		}
+	}
+
+	// last time since EOF isn't a newline
+	richestElves = keep3RichestElves(richestElves, currElfCalories)
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(richestElves)
+
+	fmt.Println(sumCalories(richestElves))
+}
