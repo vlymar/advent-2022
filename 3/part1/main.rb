d28f4dca75bda45954ba37a37c4880cb8049b5bc
Victor Lymar <victorslymar@gmail.com>
2022-12-11 14:26:09 -0800

    lint ruby

--- 3/part1/main.rb
+++ 3/part1/main.rb
@@ -10,32 +10,32 @@ require 'set'
 # return some of scores for all duplicates
 
 def detect_duplicate(line)
-    comp_size = line.length / 2
-    comp1 = line[0...comp_size].split("").to_set
-    comp2 = line[comp_size..-1].split("").to_set
-    (comp1 & comp2).first
+  comp_size = line.length / 2
+  comp1 = line[0...comp_size].split('').to_set
+  comp2 = line[comp_size..].split('').to_set
+  (comp1 & comp2).first
 end
 
 def score_token(token)
-    ascii_num = token.bytes.first
-    if /[[:upper:]]/.match(token)
-        (ascii_num - 38)
-    else
-        ascii_num - 96
-    end
+  ascii_num = token.bytes.first
+  if /[[:upper:]]/.match(token)
+    (ascii_num - 38)
+  else
+    ascii_num - 96
+  end
 end
 
 def main
-    total = 0
-    File.open("../input.txt", "r") do |f|
-        f.each_line do |line|
-            dup = detect_duplicate line
-            score = score_token(dup)
-            total += score
-        end
+  total = 0
+  File.open('../input.txt', 'r') do |f|
+    f.each_line do |line|
+      dup = detect_duplicate line
+      score = score_token(dup)
+      total += score
     end
+  end
 
-    puts total
+  puts total
 end
 
 main
